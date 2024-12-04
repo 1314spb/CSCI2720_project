de5792cf06db0aa38877be205906960ad420a623
1314-spb <leosinchungho@gmail.com>
2024-12-04 13:58:08 +0800

    NavBar added

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,18 +1,27 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css'
 
+import NavBar from './components/NavBar';
+
+import Home from './components/Home/Home';
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className="flex items-center justify-center h-screen bg-gray-100">
-      <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md space-y-4">
-        <h1 className="text-xl font-bold">Hello, Tailwind!</h1>
-        <p className="text-gray-500">這是使用 Tailwind CSS 的示例。</p>
+    <Router>
+      <div className='w-full'>
+        <NavBar />
+        <main className="flex flex-col items-center justify-center h-screen w-full">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            {/* <Route path="/about" element={<About />} />
+            <Route path="/services" element={<Services />} />
+            <Route path="/contact" element={<Contact />} /> */}
+          </Routes>
+        </main>
       </div>
-    </div>
+    </Router>
   );
 }
 
