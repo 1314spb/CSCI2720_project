c63ceaef95fbbe16ec13c2afa5a4d39fe4de8b2e
1314-spb <leosinchungho@gmail.com>
2024-12-04 12:16:21 +0800

    add tailwindCSS

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -7,28 +7,13 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md space-y-4">
+        <h1 className="text-xl font-bold">Hello, Tailwind!</h1>
+        <p className="text-gray-500">這是使用 Tailwind CSS 的示例。</p>
       </div>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
 export default App
