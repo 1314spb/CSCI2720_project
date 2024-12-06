f2624a94f7ed9266742d6df59de6336bf7f5976c
1314spb <1155174349@link.cuhk.edu.hk>
2024-12-07 00:58:26 +0800

    Merging

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { useLocation } from 'react-router-dom';
-import Dashboard from './Dashboard';
 import ProtectedRoute from './ProtectedRoute';
 import { useState } from 'react'
 import './App.css'
