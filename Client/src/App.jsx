3ae07eeadaf4678807f73fdc350f24f51cb65eca
1314spb <1155174349@link.cuhk.edu.hk>
2024-12-07 13:07:19 +0800

    Frontend - EventsManager.jsx built

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -10,6 +10,7 @@ import List_of_Events from './components/List_of_Events/List_of_Events';
 import Map from './components/Map/Map';
 import Login from './components/Login/Login';
 import UsersManager from './components/UsersManager/UsersManager';
+import EventsManager from './components/EventsManager/EventsManager';
 import Favourite from './components/Favourite/Favourite';
 
 function App() {
@@ -18,24 +19,26 @@ function App() {
       <Main />
     </Router>
   )
-  }
+}
 function Main() {
   const location = useLocation();
-  return ( 
-      <div className='w-full'>
-        {location.pathname !== '/login' && <NavBar />}
-        <main className="flex flex-col items-center justify-center h-screen w-full">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/list_of_location" element={<List_of_Location />} />
-            <Route path="/list_of_events" element={<List_of_Events />} />
-            <Route path="/map" element={<Map />} />
-            <Route path="/favourite" element={<Favourite />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/usersmanager" element={<UsersManager />} />
-          </Routes>
-        </main>
-      </div>
+  return (
+    <div className='w-full'>
+      {location.pathname !== '/login' && <NavBar />}
+      <main className="flex flex-col items-center justify-center h-screen w-full">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/list_of_location" element={<List_of_Location />} />
+          <Route path="/list_of_events" element={<List_of_Events />} />
+          <Route path="/map" element={<Map />} />
+          <Route path="/favourite" element={<Favourite />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/usersmanager" element={<UsersManager />} />
+          
+          <Route path="/eventsmanager" element={<EventsManager />} />
+        </Routes>
+      </main>
+    </div>
   );
 }
 
