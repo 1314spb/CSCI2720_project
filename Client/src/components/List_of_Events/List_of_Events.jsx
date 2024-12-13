fd4754df146a5e082a666cece4c268e46bf65b9c
timmeix <162025684+timmeix@users.noreply.github.com>
2024-12-13 23:46:20 +0800

    updated

--- Client/src/components/List_of_Events/List_of_Events.jsx
+++ Client/src/components/List_of_Events/List_of_Events.jsx
@@ -7,83 +7,88 @@ import {
     Button,
     CardBody,
     CardFooter,
-    IconButton,
-    Tooltip,
 } from "@material-tailwind/react";
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
-const TABLE_HEAD = ["Date", "Venue", "Title", "Price", "Add to Favourite"];
+const TABLE_HEAD = ["Venue", "Date", "Title", "Price", "Like"];
 
 const ListOfEvents = () => {
+    const [venues, setVenues] = useState([]);
     const [events, setEvents] = useState([]);
     const [rowsLimit] = useState(10);
     const [rowsToShow, setRowsToShow] = useState([]);
     const [currentPage, setCurrentPage] = useState(0);
     const [search, setSearch] = useState('');
-    const [sortOrder, setSortOrder] = useState({ column: 'date', direction: 'asc' });
-    const [selectedArea, setSelectedArea] = useState('default');
+    const [sortOrder, setSortOrder] = useState({ column: 'like', direction: 'asc' });
+    const [likedEvents, setLikedEvents] = useState(new Set());
 
     useEffect(() => {
         const fetchEvents = async () => {
             try {
-        //         const response = await fetch('/events.xml');
-        //         if (!response.ok) {
-        //             throw new Error(`HTTP error! status: ${response.status}`);
-        //         }
-        //         const text = await response.text();
-        //         const parser = new DOMParser();
-        //         const xml = parser.parseFromString(text, 'application/xml');
-        //         const eventList = Array.from(xml.getElementsByTagName('event')).map(event => ({
-        //             id: event.getAttribute('id'),
-        //             title: event.getElementsByTagName('titlee')[0]?.textContent || 'N/A',
-        //             venueId: event.getElementsByTagName('venueid')[0]?.textContent || 'N/A',
-        //             date: event.getElementsByTagName('predateE')[0]?.textContent || 'N/A',
-        //             price: event.getElementsByTagName('pricee')[0]?.textContent || 'N/A'
-        //         }));
-        //         setEvents(eventList);
-        //     } catch (error) {
-        //         console.error("Loading events.xml error:", error);
-        //     }
-        // };
-
-        const eventsResponse = await axios.get('http://localhost:3000/api/user/event', {
-            withCredentials: true,
-          });
-        
-            console.log(eventsResponse.data);
-            const allEvents = eventsResponse.data;
-            const eventList = allEvents.map(event => ({
+                const eventsResponse = await axios.get('http://localhost:3000/api/user/event', {
+                    withCredentials: true,
+                });
+                const allEvents = eventsResponse.data;
+                const eventList = allEvents.map(event => ({
                     id: event.eventId,
                     title: event.title || 'N/A',
                     venueId: event.locId || 'N/A',
                     date: event.datetime || 'N/A',
-                    price: event.price|| 'N/A'
-            }))
-            setEvents(eventList);
-        }catch(error){
-            console.error('Error fetching events:', error);
-        }
-        }
+                    price: event.price || 'N/A'
+                }));
+                setEvents(eventList);
+            } catch (error) {
+                console.error('Error fetching events:', error);
+            }
+        };
         
         fetchEvents();
     }, []);
 
     useEffect(() => {
+        const fetchVenues = async () => {
+            try {
+                const locationsResponse = await axios.get('http://localhost:3000/api/user/location', {
+                    withCredentials: true,
+                });
+
+                const venueList = locationsResponse.data.map(venue => ({
+                    id: venue.locId,
+                    venuelatitude: venue.lat,
+                    venuelongitude: venue.long,
+                }));
+                setVenues(venueList);
+            } catch (error) {
+                console.error('Error fetching locations or user favorites:', error);
+            }
+        };
+        
+        fetchVenues();
+    }, []);
+
+    useEffect(() => {
         const startIndex = currentPage * rowsLimit;
         const filteredEvents = events.filter(item =>
-            item.title.toLowerCase().includes(search.toLowerCase()) &&
-            (selectedArea === 'default' || item.venueId === selectedArea)
+            item.title.toLowerCase().includes(search.toLowerCase())
         );
 
         const sortedEvents = [...filteredEvents].sort((a, b) => {
+            const isALiked = likedEvents.has(a.id) ? 1 : 0;
+            const isBLiked = likedEvents.has(b.id) ? 1 : 0;
+
+            if (sortOrder.column === 'like') {
+                return sortOrder.direction === 'asc' ? isBLiked - isALiked : isALiked - isBLiked;
+            }
+
             const aValue = sortOrder.column === 'price' ? parseFloat(a.price) || 0 : a[sortOrder.column];
             const bValue = sortOrder.column === 'price' ? parseFloat(b.price) || 0 : b[sortOrder.column];
 
-            return sortOrder.direction === 'asc' ? aValue > bValue ? 1 : -1 : aValue < bValue ? 1 : -1;
+            return sortOrder.direction === 'asc' ? aValue - bValue : bValue - aValue;
         });
 
         setRowsToShow(sortedEvents.slice(startIndex, startIndex + rowsLimit));
-    }, [events, currentPage, rowsLimit, search, sortOrder, selectedArea]);
+    }, [events, currentPage, rowsLimit, search, sortOrder, likedEvents]);
 
     const totalFilteredPages = useMemo(() => {
         const filteredEvents = events.filter(item =>
@@ -109,45 +114,61 @@ const ListOfEvents = () => {
         setCurrentPage(0);
     };
 
-    const handleSort = (column) => {
-        const newDirection = sortOrder.column === column && sortOrder.direction === 'asc' ? 'desc' : 'asc';
-        setSortOrder({ column, direction: newDirection });
+    const handleSort = () => {
+        const newDirection = sortOrder.direction === 'asc' ? 'desc' : 'asc';
+        setSortOrder({ column: 'like', direction: newDirection });
         setCurrentPage(0);
     };
 
+    const toggleLike = (id) => {
+        setLikedEvents((prev) => {
+            const newLikedEvents = new Set(prev);
+            if (newLikedEvents.has(id)) {
+                newLikedEvents.delete(id);
+            } else {
+                newLikedEvents.add(id);
+            }
+            return newLikedEvents;
+        });
+    };
+
     return (
-        <div className="min-h-screen h-full flex items-center bg-gray-100">
-            <div className="w-full max-w-5xl px-4">
-                <div className="flex space-x-5 mb-4">
-                    <input
-                        type="text"
-                        placeholder="Search by title..."
-                        value={search}
-                        onChange={handleSearch}
-                        className="border border-gray-400 rounded-md p-2 w-60 text-gray-900"  // Dark text
-                    />
-                </div>
+        <div className="min-h-screen h-full flex flex-col p-4 md:p-6">
+            <div className="flex-grow">
                 <Card className="h-full w-full bg-white shadow-lg">
-                    <CardBody className="overflow-scroll px-0">
-                        <table className="mt-4 w-full table-fixed text-left">
+                    <CardBody className="overflow-auto">
+                        <div className="flex flex-col md:flex-row mb-4">
+                            <input
+                                type="text"
+                                placeholder="Search by title..."
+                                value={search}
+                                onChange={handleSearch}
+                                className="border border-gray-400 rounded-md p-2 w-60 text-gray-900"
+                            />
+                        </div>
+                        <table className="min-w-full table-fixed text-left">
                             <thead>
                                 <tr>
-                                    {TABLE_HEAD.map((head) => (
-                                        <th className="border-b border-gray-200 bg-gray-50 p-4" key={head}>
-                                            <Button
-                                                onClick={() => handleSort(head.toLowerCase())}
-                                                className="flex items-center gap-2 text-gray-900"
-                                                aria-label={`Sort by ${head.toLowerCase()}`}
-                                            >
-                                                {head}
-                                                <ChevronUpDownIcon strokeWidth={2} className="h-4 w-4" />
-                                            </Button>
+                                    {TABLE_HEAD.map((head, index) => (
+                                        <th className="border-b border-gray-200 bg-gray-50 p-4" key={index}>
+                                            {head === "Like" ? (
+                                                <Button
+                                                    onClick={handleSort}
+                                                    className="flex items-center gap-2 text-gray-900"
+                                                    aria-label={`Sort by ${head.toLowerCase()}`}
+                                                >
+                                                    {head}
+                                                    <ChevronUpDownIcon strokeWidth={2} className="h-4 w-4" />
+                                                </Button>
+                                            ) : (
+                                                <Typography variant="small" color="gray" className="font-normal">{head}</Typography>
+                                            )}
                                         </th>
                                     ))}
                                 </tr>
                             </thead>
                             <tbody>
-                                {rowsToShow.length === 0 && (
+                                {rowsToShow.length === 0 ? (
                                     <tr>
                                         <td className="p-4 text-center" colSpan={TABLE_HEAD.length}>
                                             <Typography color="gray" className="font-normal">
@@ -155,41 +176,50 @@ const ListOfEvents = () => {
                                             </Typography>
                                         </td>
                                     </tr>
-                                )}
-                                {rowsToShow.map(({ id, date, venueId, title, price }, index) => {
-                                    const isLast = index === rowsToShow.length - 1;
-                                    const classes = isLast ? "p-4" : "p-4 border-b border-gray-200";
-                                    return (
-                                        <tr key={id}>
-                                            <td className={`${classes} break-words whitespace-normal`}>
-                                                <Typography variant="small" color="gray-900" className="font-normal">{date}</Typography>
-                                            </td>
-                                            <td className={`${classes} break-words whitespace-normal`}>
-                                                <Typography variant="small" color="gray-900" className="font-normal">{venueId}</Typography>
-                                            </td>
-                                            <td className={`${classes} break-words whitespace-normal`}>
-                                                <Typography variant="small" color="gray-900" className="font-normal">{title}</Typography>
-                                            </td>
-                                            <td className={`${classes} break-words whitespace-normal`}>
-                                                <Typography variant="small" color="gray-900" className="font-normal">{price}</Typography>
-                                            </td>
-                                            <td className={classes}>
-                                                <Tooltip content="Add to Favourite">
-                                                    <IconButton
-                                                        variant="text"
-                                                        aria-label="Add to favourite"
+                                ) : (
+                                    rowsToShow.map(({ id, date, venueId, title, price }, index) => {
+                                        const isLast = index === rowsToShow.length - 1;
+                                        const classes = isLast ? "p-4" : "p-4 border-b border-gray-200";
+                                        const isLiked = likedEvents.has(id);
+
+                                        const venue = venues.find(v => v.id === venueId);
+                                        const venuelatitude = venue ? venue.venuelatitude : null;
+                                        const venuelongitude = venue ? venue.venuelongitude : null;
+
+                                        return (
+                                            <tr key={id} className="hover:bg-gray-200">
+                                                <td className={`${classes} break-words whitespace-normal`}>
+                                                    <Link to={`/map?lat=${venuelatitude}&lng=${venuelongitude}&status=true`} className="inline-block bg-slate-200 text-white font-semibold py-2 px-4 rounded-md hover:bg-slate-300 transition-colors duration-200">
+                                                        <Typography variant="small" color="gray" className="font-normal">{venueId}</Typography>
+                                                    </Link>
+                                                </td>
+                                                <td className={`${classes} break-words whitespace-normal`}>
+                                                    <Typography variant="small" color="gray-900" className="font-normal">{date}</Typography>
+                                                </td>
+                                                <td className={`${classes} break-words whitespace-normal`}>
+                                                    <Typography variant="small" color="gray-900" className="font-normal">{title}</Typography>
+                                                </td>
+                                                <td className={`${classes} break-words whitespace-normal`}>
+                                                    <Typography variant="small" color="gray-900" className="font-normal">{price}</Typography>
+                                                </td>
+                                                <td className={classes}>
+                                                    <button
+                                                        type="button"
+                                                        onClick={() => toggleLike(id)}
+                                                        className={`border rounded-lg text-sm p-2.5 inline-flex items-center me-2 ${isLiked ? 'bg-blue-700 text-white' : 'text-blue-700 border border-blue-700 hover:bg-blue-700 hover:text-white'} focus:ring-4 focus:outline-none focus:ring-blue-300`}
                                                     >
-                                                        <PencilIcon className="h-4 w-4 text-gray-800" />
-                                                    </IconButton>
-                                                </Tooltip>
-                                            </td>
-                                        </tr>
-                                    );
-                                })}
+                                                        <PencilIcon className="h-5 w-5" />
+                                                        <span className="sr-only">Like</span>
+                                                    </button>
+                                                </td>
+                                            </tr>
+                                        );
+                                    })
+                                )}
                             </tbody>
                         </table>
                     </CardBody>
-                    <CardFooter className="flex items-center justify-between border-t border-gray-200 p-4 bg-gray-50">
+                    <CardFooter className="flex flex-col md:flex-row items-center justify-between border-t border-gray-200 p-4 bg-gray-50">
                         <div className="flex items-center space-x-2">
                             <Button
                                 className='text-gray-900'
