a6af113bf44c3107646c0bacf73b2a32ed94f8bc
1314spb <1155174349@link.cuhk.edu.hk>
2024-12-17 12:52:44 +0800

    Frontend - Home.jsx updated

--- Client/src/components/Home/Home.jsx
+++ Client/src/components/Home/Home.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 import Gallery from "./Gallery";
-import NavBar from "../NavBar/NavBar";
 
 const Home = () => {
 	return (
-		<div className="flex flex-col items-center justify-center h-screen w-full">
-			{/* <NavBar/> */}
-			<Gallery />
-		</div>
+		<section class="py-8 z-10 font-serif">
+			<div class="flex flex-col md:flex-row items-center max-w-6xl px-6 py-8 mx-auto">
+				<div class="w-full md:w-1/2 py-8">
+					<h1 class="text-purple-900 text-7xl font-semibold leading-none tracking-tighter">
+						Welcome to <br /><span class="text-blue-500">Our project, <br /></span> Please take a look.
+					</h1>
+				</div>
+				<div class="w-full md:w-1/2 py-8">
+					<img src="https://www.svgrepo.com/show/493509/person-who-invests.svg" class="g-image" />
+				</div>
+			</div>
+		</section>
+
 	);
 };
 
