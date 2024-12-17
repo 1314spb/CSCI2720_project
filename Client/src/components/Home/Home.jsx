3a6be0783cca1c505866844cb303bf99ce5dcb18
1314spb <1155174349@link.cuhk.edu.hk>
2024-12-17 13:05:52 +0800

    Home title update

--- Client/src/components/Home/Home.jsx
+++ Client/src/components/Home/Home.jsx
@@ -5,8 +5,8 @@ const Home = () => {
 		<section class="py-8 z-10 font-serif">
 			<div class="flex flex-col md:flex-row items-center max-w-6xl px-6 py-8 mx-auto">
 				<div class="w-full md:w-1/2 py-8">
-					<h1 class="text-purple-900 text-7xl font-semibold leading-none tracking-tighter">
-						Welcome to <br /><span class="text-blue-500">Our project, <br /></span> Please take a look.
+					<h1 class="text-purple-900 text-7xl font-semibold leading-none tracking-tighter select-none">
+						Welcome to <br /><span class="text-blue-500">Our project, <br /></span> Please take a look
 					</h1>
 				</div>
 				<div class="w-full md:w-1/2 py-8">
